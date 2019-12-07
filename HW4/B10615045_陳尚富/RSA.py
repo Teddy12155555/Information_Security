242453a8574eac7d369612c7970b35e4ad2305cb
Shung <jordan880805@gmail.com>
2019-12-08 00:37:38 +0800

    HW4 done

--- HW4/B10615045_陳尚富/RSA.py
+++ HW4/B10615045_陳尚富/RSA.py
@@ -1,5 +1,13 @@
 import sys
 import random
+import numpy as np
+
+test_round = 40
+FOLDER = './MsgAndKey/'
+PRIVATE_KEY = FOLDER + 'private_key.txt'
+PUBLIC_KEY = FOLDER + 'public_key.txt'
+ENC_MESSAGE = FOLDER + 'enc_message.txt'
+DEC_MESSAGE = FOLDER + 'dec_message.txt'
 
 # ================ Miller Robin Test ========================
 # It returns false if n is composite 
@@ -12,7 +20,7 @@ import random
 # 2. a^((2^r)*d) mod n != n-1 
 # Then n is not prime
 # ===========================================================
-def Miller_Robin_test(N, Times):
+def Miller_Robin_test(N, Times=test_round):
 
     if N == 2 or N==3:
         return True
@@ -41,6 +49,15 @@ def Miller_Robin_test(N, Times):
         
     return True  # probably prime
 
+# Random number with bits to get probably prime by using Miller Robin test
+def Probably_Prime(bits):
+
+    p = random.getrandbits(bits-1) + Square_and_Multiply(2, bits-1)
+    while Miller_Robin_test(p, test_round) == False:
+        p = random.getrandbits(bits-1) + Square_and_Multiply(2,bits-1)
+    
+    return p
+
 # ============== Square and Miltiply Algorithm=================
 # Following steps needs to be carried out :
 #
@@ -60,23 +77,178 @@ def Square_and_Multiply(base, exp):
             value = value * value * base
         elif i == '0':  # square
             value = value * value
-
     return value
 
-def Decrypt():
-    pass
+# Square_and_Multiply with mod
+def SM_mod(base, exp, modulus):
+    result = 1
+    while exp:
+        exp, d = exp // 2, exp % 2    # shift right
+        if d:   # last bit is '1': do square and multiply, '0': square
+            result = result * base % modulus    # multiply
+        base = base * base % modulus    # square
+    return result
 
-def Encrypt():
-    pass
+# Uses Euler theorem and Square_and_Multiply
+def modPow(base, exp, modulus):
+    # do Euler
+    if Miller_Robin_test(modulus):   # do when modulu is prime (too complex when is not)
+        if base%modulus != 0 and modulus%base != 0:  # not the multiply of eachother
+            exp = exp % (modulus-1) # exp mod phi(modulu)
+        
+    return SM_mod(base,exp,modulus)
 
-# Get probably prime by using Miller Robin test
-def Probably_Prime(bits):
-    p = random.getrandbits(bits)
-    while Miller_Robin_test(p, 40) == False:
-        p = random.getrandbits(bits)
-    print('Probably prime:', p)
+
+def Chinese_Remainder(base, exp, M, m):
+    # m: prime number , M: m1*m2*...*mn
+    result = 0
+
+    for i in range(len(m)):
+        mi = m[i]
+        ai = SM_mod(base, exp, mi)  # base ^ exp = x
+        
+        Mi = M // mi
+        r, yi, _ = Extended_GCD(Mi, mi)
+        yi = yi % mi
+
+        result = (result + ai * Mi * yi)%M
     
-    return p
+    return result % M
+
+# Return (g, x, y) such that a*x + b*y = g = gcd(a, b)
+def Extended_GCD(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, x, y = Extended_GCD(b % a, a)
+        return (g, y - (b // a) * x, x)
+
+# Return the greatest common divisor of n and m.
+def GCD(n,m):
+    # m: mod number
+    # n: beeing mod
+    while  m > 0:
+        temp = m
+        m = n % m
+        n = temp
+    return n         
+
+# Algorithm to turn text into blocks
+def textToBlocks(infileName, N):
+    
+    block_size = (len(bin(N)[2:]) // 7)-1  # make sure message bits is less then N bits
+
+    # Read Message file
+    infile = open(infileName, 'r')
+    fileString = ""
+    line = infile.readline()
+    while line != "":
+        fileString += line
+        line = infile.readline()
+    
+    blockList = []
+    while len(fileString) != 0:
+        s = ""
+        s = fileString[0:block_size]    # takes a block size a time
+        fileString = fileString[block_size:]    # shift the filestring
+
+        block = 0
+        for ch in s:
+            block = block * 128 + ord(ch)   # 固定每個字為7個bit，陸續接在尾巴
+
+        blockList.append(str(block)+"\n")   # A block is a row
+
+    blockList[-1] = blockList[-1][:-1]  # '\n' not needed in last message block
+
+    # Output file
+    outfile = open(ENC_MESSAGE, 'w')
+    for item in blockList:
+        outfile.write("%s" % item)
+    outfile.close()
+
+# Algorithm to turn blocks into text
+def blocksToText(infile):
+    # Open file
+    intList = open(infile).readlines()
+
+    s = ""
+    for i in intList:
+        block = ""
+        while int(i) > 0:
+            ch = chr(int(i) % 128)    # take the last block
+            block = ch + block  # from back to head
+            i = int(i)//128     # delete the last block
+        s += block
+    
+    # Output file
+    outfile = open(infile, 'w')
+    outfile.write(s)
+    outfile.close()
+    return s
+
+# Generate public/private key file
+# private mode =1, when generate private key file
+def gen_keyfile(privateMode,n, key,filename, p=0, q=0):
+    outfile1 = open(filename, 'w')
+    outfile1.write("N:"+str(n)+"\nK:"+str(key))
+    if privateMode: # generate private key file
+        outfile1.write("\np:"+str(p)+"\nq:"+str(q))
+    
+# Unpack the keyfile and get the key
+def take_key(filename):
+    keylist = open(filename).readlines()
+    n = int((keylist[0])[2:])
+    k = int((keylist[1])[2:])
+    if len(keylist) == 2:
+        return n,k
+    else:   # private key got p q as well
+        p = int((keylist[2])[2:])
+        q = int((keylist[3])[2:])
+        return n,k,p,q
+
+
+# python3 RSA.py -d
+def Decrypt():
+    ## Decrypt the file we Encrypt previous
+    n,d,p,q = take_key(PRIVATE_KEY)
+
+    dec = []
+    enc_msg = open(ENC_MESSAGE).readlines() # Read the file we Encrypt previous
+    for C in enc_msg:
+        M = Chinese_Remainder(int(C),d,n,(p,q)) # Decrypt
+        # M = pow(int(C),d,n)
+        # M = modPow(int(C),d,n)
+        dec.append(M)
+    
+    # Output file
+    temp = open(DEC_MESSAGE, 'w')
+    for i in dec:
+        temp.write("%s\n" % str(i))
+    temp.close()
+    print(blocksToText(DEC_MESSAGE))
+
+
+# python3 RSA.py -e input_file
+def Encrypt():
+    # Take message file and public key
+    message_file = sys.argv[2]
+    n,e = take_key(PUBLIC_KEY)
+    
+    # Encrypt
+    textToBlocks(message_file, n)   # Make message to block
+    txtblk = open(ENC_MESSAGE).readlines()  # Read output file made in testToBlocks()
+    enc = []
+    for M in txtblk:
+        C = pow(int(M),e,n) # Encrypt
+        enc.append(C)
+
+    # Output file
+    outfile = open(ENC_MESSAGE,'w')
+    for i in enc:
+        print(i)
+        outfile.write("%s\n" % str(i))
+    outfile.close()
+
 
 def Init():
 
@@ -84,23 +256,48 @@ def Init():
     # Get Probably prime
     p = Probably_Prime(bits)
     q = Probably_Prime(bits)
+
+    # Get N and phi N
     N = p * q
     phi_N = (p-1)*(q-1)
     
+    # Get e
+    while True:
+        e = random.randrange(2, bits*100)
+        if GCD(e, phi_N) == 1:
+            break
 
-# TEST
+    # Get d
+    r,d,y = Extended_GCD(e, phi_N)
+    d = d % phi_N
     
-    print(bits)
-    print('N: ', N)
-    # if Miller_Robin_test(1049,40):
-    #     print('yeee')
-    # print(Square_and_Multiply(5, 256))
-    # print(pow(5,256))
+    print('private key(\nN:\n{},\nd:\n{})\n\npublic key(\nN:\n{},\ne:\n{})\n'.format(N, d, N, e))
+
+    # Generate public key file
+    gen_keyfile(0, N, e, PUBLIC_KEY)
+    # Generate private key file
+    gen_keyfile(1, N, d, PRIVATE_KEY, p, q)
+    
+# TEST
+    print('N: \n{}'.format(N))
+    print('p: \n{}\nq: \n{}'.format(p,q))
+
+    print(Chinese_Remainder_forRSA(894,1,1001,(7,11,13)))
+    print(Chinese_Remainder_forRSA(17639,11613,21829,(83,263)))
+    
+    if modPow(147882322,e,N)!=pow(147882322,e,N):
+        print(pow(147882322,e,N))
+        print(modPow(147882322,e,N))
+
+    if pow(147882322,d,N)!=modPow(147882322,d,N):
+        print(pow(147882322,d,N))
+        print(modPow(147882322,d,N))
+        
 
 
 if __name__ == "__main__":
 
-    try:
+    # try:
         # Input processing
         if sys.argv[1] == 'init':   # return p,q,n,e,d
             Init()
@@ -110,10 +307,9 @@ if __name__ == "__main__":
 
         elif sys.argv[1] == '-d':   # decrypt
             Decrypt()
+    #     else:
+    #         raise
 
-        else:
-            raise
-
-    except:
-        print('Error: Input Format')
-        pass
+    # except:
+    #     print('Error: Input Format')
+    #     pass
