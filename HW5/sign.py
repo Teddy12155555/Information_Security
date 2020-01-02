c0cdb659896a9c20f0f5c7f14579baa8e2479acb
Teddy12155555 <mickey23405383@gmail.com>
2020-01-02 21:18:07 +0800

    Fix

--- HW5/sign.py
+++ HW5/sign.py
@@ -12,27 +12,23 @@ def sign():
 		file0 = open(fileName,"r")
 		file1 = open("public_key.txt","r")
 		file2 = open("private_key.txt","r")
-        
 		p = int(file1.readline().rstrip())
 		q = int(file1.readline().rstrip())
 		a = int(file1.readline().rstrip())
 		b = int(file1.readline().rstrip())
 		d = int(file2.readline().rstrip())
-        string = file0.read()
+		string = file0.read()
 
-        Ke = random.randint(1,q)
-        KeInv = Inverse(Ke,q)
-        SHA = sha1(string.encode('utf-8')).hexdigest()
-        hex = "0x"+SHA
-        #print(int(hex,0))
-        #print(int(hex,0).bit_length())
-        r = mod_Square_and_Multiply(a,Ke,p)
-        r = r % q
-        s = ((int(hex,0) + (d * r)) * KeInv) % q
-        #output signed file
-        SignFile = open("signature.txt","w")
-        SignFile.write(str(r)+"\n"+str(s)+"\n")
-        SignFile.close()
-        print ('sign complete')
-        
+		Ke = random.randint(1,q)
+		KeInv = Inverse(Ke,q)
+		SHA = sha1(string.encode('utf-8')).hexdigest()
+		hex = "0x"+SHA
+
+		r = mod_Square_and_Multiply(a,Ke,p)
+		r = r % q
+		s = ((int(hex,0) + (d * r)) * KeInv) % q
+		SignFile = open("signature.txt","w")
+		SignFile.write(str(r)+"\n"+str(s)+"\n")
+		SignFile.close()
+		print ('sign complete')
 sign()
